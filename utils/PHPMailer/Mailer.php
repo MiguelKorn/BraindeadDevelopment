52f92558db6d2d511251b95fb2bfadd1b6552805
Miguel Korn <20448@ma-web.nl>
2017-09-26 12:26:06 +0200

    [ADD] backend contact form

--- utils/PHPMailer/Mailer.php
+++ utils/PHPMailer/Mailer.php
@@ -1,60 +1,60 @@
 <?php
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require_once 'Exception.php';
+require_once 'PHPMailer.php';
+require_once 'SMTP.php';
 
 class Mailer
 {
     private $mail;
 
-    private $mailFromAddress;
-    private $mailFromName;
-    private $mailFromPass;
-
-    private $mailToAddress;
-    private $mailToName;
-
-    private $subject;
-    private $body;
-
-    function __construct($fromName, $fromAddress, $fromPass, $toAddress, $toName, $subject, $body)
+    function __construct($smtpUsername, $smtpPassword)
     {
-        $this->mail = new PHPMailer(true);// Passing `true` enables exceptions
+        $this->mail = new PHPMailer();// Passing `true` enables exceptions
 
-        $this->mailFromName = $fromName;
-        $this->mailFromAddress = $fromAddress;
-        $this->mailFromPass = $fromPass;
-
-        $this->mailToAddress = $toAddress;
-        $this->mailToName = $toName;
-
-        $this->subject = $subject;
-        $this->body = $body;
-
-        $this->setSettings();
-    }
-
-    function setSettings() {
         //Server settings
-        $this->mail->SMTPDebug = 2;                                   // Enable verbose debug output (0 = production)
+        $this->mail->SMTPDebug = 0;                                   // Enable verbose debug output (0 = production)
         $this->mail->isSMTP();                                        // Set mailer to use SMTP
         $this->mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
         $this->mail->SMTPAuth = true;                                 // Enable SMTP authentication
-        $this->mail->Username = $this->mailFromAddress; // SMTP username
-        $this->mail->Password = $this->mailFromPass;                             // SMTP password
+        $this->mail->Username = $smtpUsername;                        // SMTP username
+        $this->mail->Password = $smtpPassword;                        // SMTP password
         $this->mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
         $this->mail->Port = 587;                                      // TCP port to connect to
 
         //Recipients
-        $this->mail->setFrom($this->mailFromAddress, $this->mailFromName);
-        $this->mail->addAddress($this->mailToAddress, $this->mailToName);     // Add a recipient
 //            $this->mail->addReplyTo('info@example.com', 'Information');
 
         //Content
         $this->mail->isHTML(true);                                  // Set email format to HTML
-        $this->mail->Subject = 'Here is the subject';
-        $this->mail->Body = 'This is the HTML message body <b>in bold!</b>';
-        $this->mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    }
+
+    public function setMailFrom($mailFromAddress, $mailFromName)
+    {
+        $this->mail->setFrom($mailFromAddress, $mailFromName);
+    }
+
+    public function setMailTo($mailToAddress, $mailToName)
+    {
+        $this->mail->addAddress($mailToAddress, $mailToName);
+    }
+
+    public function setSubject($subject)
+    {
+        $this->mail->Subject = $subject;
+    }
+
+    public function setBody($body)
+    {
+        $this->mail->Body = $body;
+    }
+
+    public function setAltBody($altBody)
+    {
+        $this->mail->AltBody = $altBody;
     }
 
     function sendMail()
